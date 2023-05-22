892002c5c20b686f91cced5afeed20b6690e10a7
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-22 17:05:28 -0500

    index clientes

--- Pages/Clientes/Index.cshtml.cs
+++ Pages/Clientes/Index.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using TrasladoSeguro.Data;
+using TrasladoSeguro.Migrations;
+using TrasladoSeguro.Models;
 
 namespace TrasladoSeguro.Pages.Clientes
 {
     public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
+    {        
+            private readonly TransporteSeguroContext _context;
+
+            public IndexModel(TransporteSeguroContext context)
+            {
+                _context = context;
+            }
+
+            public IList<Cliente> Clientes { get; set; } = default!;
+
+            public async Task OnGetAsync()
+            {
+                if (_context.Clientes != null)
+                {
+                    Clientes = await _context.Clientes.ToListAsync();
+                }
+            }
     }
 }
+
