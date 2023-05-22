313db9abfb71105b83b4ab5d0b88bf3ea42337a8
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-22 15:59:33 -0500

    Migrations y database

---
+++ Data/TransporteSeguroContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrasladoSeguro.Models;
+
+namespace TrasladoSeguro.Data
+{
+    public class TransporteSeguroContext: DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer(
+            "Server=(Localdb)\\mssqllocaldb;Database=TransporteSeguro;Trusted_Connection=True;");
+        }
+
+    }
+}
