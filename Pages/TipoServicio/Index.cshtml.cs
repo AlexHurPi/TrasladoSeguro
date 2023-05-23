637c159e3e6e243877a3f843595fc6bf02497ba2
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-22 21:52:25 -0500

    Index type of service

---
+++ Pages/TipoServicio/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using TrasladoSeguro.Data;
+using TrasladoSeguro.Models;
+
+namespace TrasladoSeguro.Pages.TipoServicio
+{
+    public class IndexModel : PageModel
+    {        
+            private readonly TransporteSeguroContext _context;
+
+            public IndexModel(TransporteSeguroContext context)
+            {
+                _context = context;
+            }
+
+            public IList<Cliente> Clientes { get; set; } = default!;
+
+            public async Task OnGetAsync()
+            {
+                if (_context.Clientes != null)
+                {
+                    Clientes = await _context.Clientes.ToListAsync();
+                }
+            }
+    }
+}
+
