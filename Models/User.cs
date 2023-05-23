adea254ec028e9996fe213e5e3ef9fab32e0a9bb
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-23 12:06:55 -0500

    Settings to users

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,15 @@
-﻿namespace TrasladoSeguro.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrasladoSeguro.Models
 {
 	public class User
 	{
 		public int Id { get; set; }
+		[Required]//verificar que se importo using System.ComponentModel.DataAnnotations
+		[DataType(DataType.EmailAddress)]
 		public string Email { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
 		public string Password { get; set; }
 	}
 }
