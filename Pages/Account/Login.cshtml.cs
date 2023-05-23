ca37274b7feef3b9f4a19883b4d4146e0121b388
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-23 12:36:36 -0500

    Logout

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -2,12 +2,21 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
+using TrasladoSeguro.Data;
 using TrasladoSeguro.Models;
 
 namespace TrasladoSeguro.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private readonly TransporteSeguroContext _context;
+
+        public LoginModel(TransporteSeguroContext context)
+        {
+            _context = context;
+        }
+        [TempData]
+        public string ErrorMessage { get; set; }
         [BindProperty]
         public User User { get; set; }
         public void OnGet()
@@ -16,24 +25,30 @@ namespace TrasladoSeguro.Pages.Account
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid) return Page();
-            if (User.Email == "correo@gmail.com" && User.Password == "12345")
+            foreach (var user in _context.Users)
             {
-                //se crea los claim, datos a almacenar en la cookie
-                var claims = new List<Claim>
+                var email = user.Email;
+                var password = user.Password;
+                if (User.Email == email && User.Password == password)
+                {
+                    //se crea los claim, datos a almacenar en la cookie
+                    var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, "admin"),
                     new Claim(ClaimTypes.Email, User.Email),
                 };
-                //se asocia los claims creados a un nombre de una cookie
-                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
-                //Se agrega la identidad creada al ClaimPrincipal de la aplicacion
-                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
-                // se registra exitosamente la autenticacion y se crea la cookie en el navegador
-                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
-                return RedirectToPage("/index");
+                    //se asocia los claims creados a un nombre de una cookie
+                    var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                    //Se agrega la identidad creada al ClaimPrincipal de la aplicacion
+                    ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                    // se registra exitosamente la autenticacion y se crea la cookie en el navegador
+                    await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                    return RedirectToPage("/index");
+                }
             }
+            ErrorMessage = "Correo o contraseña incorrectos.";
             return Page();
-
         }
     }
 }
+
