9b56a2a55e40a37d4447341a09e17505c6a5747e
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-23 12:46:17 -0500

    Driver model

---
+++ Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace TrasladoSeguro.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Identification { get; set; }
+    }
+}
