76e6b89d7bd5f98d64d73fe3db2fd299f297e89c
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-22 22:58:56 -0500

    Create user

---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using TrasladoSeguro.Data;
+using TrasladoSeguro.Models;
+
+namespace TrasladoSeguro.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+		private readonly TransporteSeguroContext _context;
+
+		public CreateModel(TransporteSeguroContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+		[BindProperty]
+
+		public User User { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.Users == null || User == null)
+			{
+				return Page();
+			}
+
+			_context.Users.Add(User);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
