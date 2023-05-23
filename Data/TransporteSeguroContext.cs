9bc5b93bc5aa4817780a854466a7742839acbc9c
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-23 12:46:30 -0500

    Drivers dtabase

--- Data/TransporteSeguroContext.cs
+++ Data/TransporteSeguroContext.cs
@@ -12,9 +12,10 @@ namespace TrasladoSeguro.Data
 		public DbSet<Cliente> Clientes { get; set; }
 		public DbSet<ServiceType> ServiceTypes { get; set; }
 		public DbSet<User> Users { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
 
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
            optionsBuilder.UseSqlServer(
             "Server=(Localdb)\\mssqllocaldb;Database=TransporteSeguro;Trusted_Connection=True;");
--- Migrations/20230523174256_Driver.Designer.cs
+++ Migrations/20230523174256_Driver.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrasladoSeguro.Data;
 
@@ -10,9 +11,11 @@ using TrasladoSeguro.Data;
 namespace TrasladoSeguro.Migrations
 {
     [DbContext(typeof(TransporteSeguroContext))]
-    partial class TransporteSeguroContextModelSnapshot : ModelSnapshot
+    [Migration("20230523174256_Driver")]
+    partial class Driver
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
