d98a0ced1c9d9c222116606ae3844ff51fa3854f
AlexHurPi <Alexander.hurtado7170@gmail.com>
2023-05-23 12:07:53 -0500

    Autentication

--- Pages/TipoServicio/Index.cshtml.cs
+++ Pages/TipoServicio/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -7,6 +8,7 @@ using TrasladoSeguro.Models;
 
 namespace TrasladoSeguro.Pages.TipoServicio
 {
+    [Authorize]
     public class IndexModel : PageModel
     {        
             private readonly TransporteSeguroContext _context;
